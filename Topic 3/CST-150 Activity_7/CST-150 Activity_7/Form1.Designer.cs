5da1f859748ff02949cb13a3ba19385150643b56
nof-pdx <nof2@pdx.edu>
2022-08-01 18:43:52 -0700

    Complete Activity

--- Topic 3/CST-150 Activity_7/CST-150 Activity_7/Form1.Designer.cs
+++ Topic 3/CST-150 Activity_7/CST-150 Activity_7/Form1.Designer.cs
@@ -28,20 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NumberItemsLabel = new System.Windows.Forms.Label();
+            this.NumberItemsTextbox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ApproxValueOfPi = new System.Windows.Forms.Label();
+            this.PIValue = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // NumberItemsLabel
+            // 
+            this.NumberItemsLabel.AutoSize = true;
+            this.NumberItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumberItemsLabel.Location = new System.Drawing.Point(69, 62);
+            this.NumberItemsLabel.Name = "NumberItemsLabel";
+            this.NumberItemsLabel.Size = new System.Drawing.Size(148, 20);
+            this.NumberItemsLabel.TabIndex = 0;
+            this.NumberItemsLabel.Text = "Enter # of Terms:";
+            // 
+            // NumberItemsTextbox
+            // 
+            this.NumberItemsTextbox.Location = new System.Drawing.Point(236, 62);
+            this.NumberItemsTextbox.Name = "NumberItemsTextbox";
+            this.NumberItemsTextbox.Size = new System.Drawing.Size(100, 20);
+            this.NumberItemsTextbox.TabIndex = 1;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(73, 100);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(81, 42);
+            this.CalculateButton.TabIndex = 2;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ApproxValueOfPi
+            // 
+            this.ApproxValueOfPi.AutoSize = true;
+            this.ApproxValueOfPi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ApproxValueOfPi.Location = new System.Drawing.Point(69, 166);
+            this.ApproxValueOfPi.Name = "ApproxValueOfPi";
+            this.ApproxValueOfPi.Size = new System.Drawing.Size(302, 20);
+            this.ApproxValueOfPi.TabIndex = 3;
+            this.ApproxValueOfPi.Text = "Approximate value of pi after x terms";
+            // 
+            // PIValue
+            // 
+            this.PIValue.AutoSize = true;
+            this.PIValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PIValue.Location = new System.Drawing.Point(73, 214);
+            this.PIValue.Name = "PIValue";
+            this.PIValue.Size = new System.Drawing.Size(79, 20);
+            this.PIValue.TabIndex = 4;
+            this.PIValue.Text = "= 3.1415";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 450);
+            this.ClientSize = new System.Drawing.Size(450, 277);
+            this.Controls.Add(this.PIValue);
+            this.Controls.Add(this.ApproxValueOfPi);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.NumberItemsTextbox);
+            this.Controls.Add(this.NumberItemsLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Approximate PI";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label NumberItemsLabel;
+        private System.Windows.Forms.TextBox NumberItemsTextbox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label ApproxValueOfPi;
+        private System.Windows.Forms.Label PIValue;
     }
 }
 
--- Topic 3/CST-150 Activity_7/CST-150 Activity_7/Form1.cs
+++ Topic 3/CST-150 Activity_7/CST-150 Activity_7/Form1.cs
@@ -16,5 +16,38 @@ namespace CST_150_Activity_7
         {
             InitializeComponent();
         }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            double numberOfTermsForPi;
+            double pi = 0;
+            double oddNums = 1;
+            int four = 4;
+            int two = 4;
+            try
+            {
+                numberOfTermsForPi = double.Parse(NumberItemsTextbox.Text);
+                for (int i = 0; i < numberOfTermsForPi; i++)
+                {
+
+                    if ( i % 2 == 1)
+                    {
+                        pi = pi - (four / oddNums);
+                    }
+                    else
+                    {
+                        pi = pi + (four / oddNums);
+                    }
+                    oddNums = oddNums + two;
+                }
+                ApproxValueOfPi.Text = "Approximate value of pi after " + numberOfTermsForPi + " terms";
+                PIValue.Text = "= " + pi.ToString();
+            }
+            catch
+            {
+                PIValue.Text = "= Invalid Data";
+            }
+        }
     }
 }
+
