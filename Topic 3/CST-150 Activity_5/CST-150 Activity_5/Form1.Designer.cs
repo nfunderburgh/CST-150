2059713800eb70b0cb2ef9389ae6f979f229c641
nof-pdx <nof2@pdx.edu>
2022-08-01 20:47:23 -0700

    VowelFunction

---
+++ Topic 3/CST-150 Activity_5/CST-150 Activity_5/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace CST_150_Activity_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Read_File = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Read_File
+            // 
+            this.Read_File.Location = new System.Drawing.Point(132, 212);
+            this.Read_File.Name = "Read_File";
+            this.Read_File.Size = new System.Drawing.Size(75, 23);
+            this.Read_File.TabIndex = 0;
+            this.Read_File.Text = "Read File";
+            this.Read_File.UseVisualStyleBackColor = true;
+            this.Read_File.Click += new System.EventHandler(this.Read_File_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(132, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(341, 128);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 381);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Read_File);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Read_File;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Topic 3/CST-150 Activity_5/CST-150 Activity_5/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CST_150_Activity_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Read_File_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            string[] lines = File.ReadAllLines(filename);
+            lines = lines.Select(v => v.ToLower()).ToArray();
+            
+            
+            string result = longestWord(lines);
+            string test = vowelCounter(lines);
+            
+
+        }
+
+        private string longestWord(string[] lines)
+        {
+            string longestWord = "";
+            for (int i = 0; i < lines.Count(); i ++)
+            {
+                if(lines[i].Length > longestWord.Length) {
+                    longestWord = lines[i];
+                }
+            }
+            return longestWord;
+        }
+
+        private string vowelCounter(string[] lines)
+        {
+            int vowelCount = 0;
+            string longestWord = "";
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (lines[i][j] == 'a' || lines[i][j] == 'e' || lines[i][j] == 'i' || lines[i][j] == 'o' || lines[i][j] == 'u')
+                    {
+                        vowelCount++;
+                    }
+                   
+                }
+            }
+            return longestWord;
+        }
+
+    }
+}
