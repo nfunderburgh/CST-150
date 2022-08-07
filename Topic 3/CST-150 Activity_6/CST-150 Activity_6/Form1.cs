f11668cffab29d2ab1af394f077a8a3c9d5e16ce
nof-pdx <nof2@pdx.edu>
2022-08-07 12:31:00 -0700

    Activity 6 done

---
+++ Topic 3/CST-150 Activity_6/CST-150 Activity_6/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150_Activity_2
+{
+    public partial class Title : Form
+    {
+        public Title()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ConvertBtn(object sender, EventArgs e)
+        {
+            // Formula to calulate weight on mars
+            // Weight on Mars = (Weight on Earth / 9.81) * 3.711
+            double earthsGravityForce = 9.81;
+            double marsGravityForce = 3.711;
+            try
+            {
+                double weightOnMars = double.Parse(EarthWeightTextbox.Text);
+                weightOnMars = weightOnMars / earthsGravityForce;
+                weightOnMars = weightOnMars * marsGravityForce;
+                MarsWeightTextbox.Text = weightOnMars.ToString("N3");
+            }
+            catch {
+                MarsWeightTextbox.Text = "Invalid data";
+            }
+
+        }
+    }
+}
---
+++ Topic 3/CST-150 Activity_6/README.md
@@ -0,0 +1,9 @@
+# CST-150-WeightOnMars
+By: Noah Funderburgh
+
+Loom URL: https://www.loom.com/share/e866a2aca5214fca97cb7e84cff8d938
+
+
+This program will convert you weight on earth into your weight on mars. In order to find your weight
+on mars inserts you weight on earth in the top textbox. After doing so click the convert button
+and then you weight on mars will popout in the bottom textbox.
