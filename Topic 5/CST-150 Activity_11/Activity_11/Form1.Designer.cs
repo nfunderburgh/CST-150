350cbb56705350bcb28e08ecdb46a6f928327dbb
nof-pdx <nof2@pdx.edu>
2022-08-16 19:26:09 -0700

    Activity 11

---
+++ Topic 5/CST-150 Activity_11/Activity_11/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Activity_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RollDice = new System.Windows.Forms.Button();
+            this.DiceNum1 = new System.Windows.Forms.RichTextBox();
+            this.DiceNum2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // RollDice
+            // 
+            this.RollDice.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RollDice.Location = new System.Drawing.Point(49, 50);
+            this.RollDice.Name = "RollDice";
+            this.RollDice.Size = new System.Drawing.Size(162, 138);
+            this.RollDice.TabIndex = 0;
+            this.RollDice.Text = "Roll Dice";
+            this.RollDice.UseVisualStyleBackColor = true;
+            this.RollDice.Click += new System.EventHandler(this.RollDice_Click);
+            // 
+            // DiceNum1
+            // 
+            this.DiceNum1.BackColor = System.Drawing.SystemColors.Window;
+            this.DiceNum1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiceNum1.Location = new System.Drawing.Point(282, 69);
+            this.DiceNum1.Name = "DiceNum1";
+            this.DiceNum1.Size = new System.Drawing.Size(100, 96);
+            this.DiceNum1.TabIndex = 1;
+            this.DiceNum1.Text = "";
+            // 
+            // DiceNum2
+            // 
+            this.DiceNum2.BackColor = System.Drawing.SystemColors.Window;
+            this.DiceNum2.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiceNum2.Location = new System.Drawing.Point(440, 69);
+            this.DiceNum2.Name = "DiceNum2";
+            this.DiceNum2.Size = new System.Drawing.Size(100, 96);
+            this.DiceNum2.TabIndex = 2;
+            this.DiceNum2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(591, 247);
+            this.Controls.Add(this.DiceNum2);
+            this.Controls.Add(this.DiceNum1);
+            this.Controls.Add(this.RollDice);
+            this.Name = "Form1";
+            this.Text = "Dice";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RollDice;
+        private System.Windows.Forms.RichTextBox DiceNum1;
+        private System.Windows.Forms.RichTextBox DiceNum2;
+    }
+}
+
---
+++ Topic 5/CST-150 Activity_11/Activity_11/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_11
+{
+    public partial class Form1 : Form
+    {
+        private int numSidesOnDice;
+        private int rollCounter = 0;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            numSidesOnDice = random.Next(4, 20);
+            DiceNum1.SelectionAlignment = HorizontalAlignment.Center;
+            DiceNum2.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void RollDice_Click(object sender, EventArgs e)
+        {
+            int dice1 = rollDie();
+            int dice2 = rollDie();
+            DiceNum1.Text = dice1.ToString();
+            DiceNum2.Text = dice2.ToString();
+            rollCounter++;
+            if(dice1 == 1 &&  dice2 == 1)
+            {
+                MessageBox.Show(" It took " + rollCounter.ToString() + " rolls to get snake eyes");
+            }
+        }
+
+        /************************************************************
+         * This generates a random number betwwen 1 and the number of sides on the dice
+         * @returns a randome number betwwen 1 and the number of sides on the dice
+         ************************************************************/
+        private int rollDie()
+        {
+            int die = random.Next(1, numSidesOnDice);
+            return die;
+        }
+    }
+}
