c0c2a04355b8fcc0a07e7a6aaa3721b6acdc1ec6
nof-pdx <nof2@pdx.edu>
2022-08-20 17:47:25 -0700

    Activity 10

---
+++ Topic 5/CST-150 Activity_10/ConsoleApp1/Program.cs
@@ -0,0 +1,93 @@
+﻿// Corrected by Noah Funderburgh
+
+using System;
+using System.Collections.Generic;
+using System.Linq;using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+        }
+    }
+
+    class Set
+    {
+        private List<int> elements;
+        public Set()
+        {
+            elements = new List<int>();
+        }
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) 
+                return false; 
+            else 
+            { 
+                elements.Add(val); 
+                return true; 
+            }
+        }
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                    return true;
+                // Not needed else return
+                //else
+                    //return false;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+        public Set union(Set rhs)
+        {
+            // Creating a new set to keep orignal set values
+            Set newSet = new Set();
+            // Adds rhs into set
+            for (int i = 0; i < rhs.elements.Count; i++) 
+            { 
+                newSet.addElement(rhs.elements[i]); 
+            }
+            // Then adds elements from this. if needed into the set
+            for (int x = 0; x < this.elements.Count; x++)
+            {
+                newSet.addElement(this.elements[x]);
+            }
+            return newSet;
+        }
+    }
+}
---
+++ Topic 5/CST-150 Activity_10/README.md
@@ -0,0 +1,8 @@
+# CST-150-Activity 9
+By: Noah Funderburgh
+
+Loom URL: 
+
+This is a program creates the union of two sets. I modified the union function as well as the contains element function.
+In the contain element function it had a unessecary else statement. In the union function it was putting the union set in
+A so when you would print out set A it would instead print out the union of A & B
