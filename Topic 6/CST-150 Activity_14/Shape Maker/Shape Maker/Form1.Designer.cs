79339561423312a8b5033911251e269ccf71faa8
nof-pdx <nof2@pdx.edu>
2022-08-24 12:34:21 -0700

    Activity 14

---
+++ Topic 6/CST-150 Activity_14/Shape Maker/Shape Maker/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Shape_Maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shapeLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.fillGroupBox = new System.Windows.Forms.GroupBox();
+            this.outlineRadioButton = new System.Windows.Forms.RadioButton();
+            this.fillRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dateCheckBox = new System.Windows.Forms.CheckBox();
+            this.nameCheckBox = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fillGroupBox.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // shapeLabel
+            // 
+            this.shapeLabel.AutoSize = true;
+            this.shapeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shapeLabel.Location = new System.Drawing.Point(40, 56);
+            this.shapeLabel.Name = "shapeLabel";
+            this.shapeLabel.Size = new System.Drawing.Size(97, 16);
+            this.shapeLabel.TabIndex = 0;
+            this.shapeLabel.Text = "Select a shape";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.Location = new System.Drawing.Point(361, 283);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(44, 20);
+            this.dateLabel.TabIndex = 1;
+            this.dateLabel.Text = "Date";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(361, 317);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(51, 20);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "Name";
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(292, 360);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 23);
+            this.drawButton.TabIndex = 3;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(416, 360);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // fillGroupBox
+            // 
+            this.fillGroupBox.Controls.Add(this.outlineRadioButton);
+            this.fillGroupBox.Controls.Add(this.fillRadioButton);
+            this.fillGroupBox.Location = new System.Drawing.Point(43, 161);
+            this.fillGroupBox.Name = "fillGroupBox";
+            this.fillGroupBox.Size = new System.Drawing.Size(200, 100);
+            this.fillGroupBox.TabIndex = 5;
+            this.fillGroupBox.TabStop = false;
+            this.fillGroupBox.Text = "Select fill mode";
+            // 
+            // outlineRadioButton
+            // 
+            this.outlineRadioButton.AutoSize = true;
+            this.outlineRadioButton.Location = new System.Drawing.Point(24, 52);
+            this.outlineRadioButton.Name = "outlineRadioButton";
+            this.outlineRadioButton.Size = new System.Drawing.Size(58, 17);
+            this.outlineRadioButton.TabIndex = 1;
+            this.outlineRadioButton.TabStop = true;
+            this.outlineRadioButton.Text = "Outline";
+            this.outlineRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // fillRadioButton
+            // 
+            this.fillRadioButton.AutoSize = true;
+            this.fillRadioButton.Location = new System.Drawing.Point(24, 29);
+            this.fillRadioButton.Name = "fillRadioButton";
+            this.fillRadioButton.Size = new System.Drawing.Size(37, 17);
+            this.fillRadioButton.TabIndex = 0;
+            this.fillRadioButton.TabStop = true;
+            this.fillRadioButton.Text = "Fill";
+            this.fillRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dateCheckBox);
+            this.groupBox2.Controls.Add(this.nameCheckBox);
+            this.groupBox2.Location = new System.Drawing.Point(43, 283);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select details";
+            // 
+            // dateCheckBox
+            // 
+            this.dateCheckBox.AutoSize = true;
+            this.dateCheckBox.Location = new System.Drawing.Point(24, 57);
+            this.dateCheckBox.Name = "dateCheckBox";
+            this.dateCheckBox.Size = new System.Drawing.Size(49, 17);
+            this.dateCheckBox.TabIndex = 1;
+            this.dateCheckBox.Text = "Date";
+            this.dateCheckBox.UseVisualStyleBackColor = true;
+            this.dateCheckBox.CheckedChanged += new System.EventHandler(this.dateCheckBox_CheckedChanged);
+            // 
+            // nameCheckBox
+            // 
+            this.nameCheckBox.AutoSize = true;
+            this.nameCheckBox.Location = new System.Drawing.Point(24, 34);
+            this.nameCheckBox.Name = "nameCheckBox";
+            this.nameCheckBox.Size = new System.Drawing.Size(54, 17);
+            this.nameCheckBox.TabIndex = 0;
+            this.nameCheckBox.Text = "Name";
+            this.nameCheckBox.UseVisualStyleBackColor = true;
+            this.nameCheckBox.CheckedChanged += new System.EventHandler(this.nameCheckBox_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Circle",
+            "Triangle",
+            "Rectangle",
+            "Pentagon",
+            "Hexagon"});
+            this.listBox1.Location = new System.Drawing.Point(43, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 69);
+            this.listBox1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Location = new System.Drawing.Point(292, 61);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 200);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.main_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.fillGroupBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.shapeLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.fillGroupBox.ResumeLayout(false);
+            this.fillGroupBox.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label shapeLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.GroupBox fillGroupBox;
+        private System.Windows.Forms.RadioButton outlineRadioButton;
+        private System.Windows.Forms.RadioButton fillRadioButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox dateCheckBox;
+        private System.Windows.Forms.CheckBox nameCheckBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Topic 6/CST-150 Activity_14/Shape Maker/Shape Maker/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shape_Maker
+{
+    public partial class Form1 : Form
+    {
+        bool clearGraphics = false;
+        public Form1()
+        {
+            InitializeComponent();
+            nameLabel.Text = "";
+            dateLabel.Text = "";
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            //Date
+            if (dateCheckBox.Checked)
+            {
+                DateTime thisDay = DateTime.Today;
+                dateLabel.Text = thisDay.ToString("d");
+            }
+            else
+            {
+                dateLabel.Text = "";
+            }
+            //Name
+            if (nameCheckBox.Checked)
+            {
+                nameLabel.Text = listBox1.SelectedItem.ToString();
+            }
+            else
+            {
+                nameLabel.Text = "";
+            }
+            this.Refresh();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            clearGraphics = true;
+            this.Refresh();
+            clearGraphics = false;
+        }
+
+        private void main_Paint(object sender, PaintEventArgs e)
+        {
+            string selectedShape = "";
+            if (listBox1.SelectedIndex != -1 )
+            {
+                selectedShape = listBox1.SelectedItem.ToString();
+            }
+            switch (selectedShape)
+            {
+                case "Circle":
+                    circle_Paint(this, e);
+                    break;
+                case "Triangle":
+                    triangle_Paint(this, e);
+                    break;
+                case "Rectangle":
+                    rectangle_Paint(this, e);
+                    break;
+                case "Pentagon":
+                    pentagon_Paint(this, e);
+                    break;
+                case "Hexagon":
+                    hexagon_Paint(this, e);
+                    break;
+            }
+            if(clearGraphics)
+            {
+                e.Graphics.Clear(Color.White);
+            }
+        }
+
+        private void circle_Paint(object sender, PaintEventArgs e)
+        {
+            if (outlineRadioButton.Checked)
+            {
+                Graphics g = e.Graphics;
+                Pen blackPen = new Pen(Color.Black, 4);
+                g.DrawEllipse(blackPen, 50, 50, 100, 100);
+            }
+            if (fillRadioButton.Checked)
+            {
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                e.Graphics.FillEllipse(blackBrush, 50, 50, 100, 100);
+            }
+        }
+
+        private void triangle_Paint(object sender, PaintEventArgs e)
+        {
+            if (outlineRadioButton.Checked)
+            {
+                Pen blackPen = new Pen(Color.Black, 4);
+                e.Graphics.DrawLine(blackPen, 150, 150, 100, 50);
+                e.Graphics.DrawLine(blackPen, 100, 50, 50, 150);
+                e.Graphics.DrawLine(blackPen, 150, 150, 50, 150);
+            }
+            if (fillRadioButton.Checked)
+            {
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                Point point1 = new Point(150, 150);
+                Point point2 = new Point(50, 150);
+                Point point3 = new Point(100, 50);
+                Point[] curvePoints = { point1, point2, point3 };
+                e.Graphics.FillPolygon(blackBrush, curvePoints);
+            }
+        }
+
+        private void rectangle_Paint(object sender, PaintEventArgs e)
+        {
+            if (outlineRadioButton.Checked)
+            {
+                Pen blackPen = new Pen(Color.Black, 4);
+                e.Graphics.DrawLine(blackPen, 50, 50, 150, 50);
+                e.Graphics.DrawLine(blackPen, 50, 50, 50, 150);
+                e.Graphics.DrawLine(blackPen, 150, 150, 50, 150);
+                e.Graphics.DrawLine(blackPen, 150, 150, 150, 50);
+            }
+            if (fillRadioButton.Checked)
+            {
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                Rectangle rect = new Rectangle(50, 50, 100, 100);
+                e.Graphics.FillRectangle(blackBrush, rect);
+            }
+        }
+
+        private void pentagon_Paint(object sender, PaintEventArgs e)
+        {
+            if (outlineRadioButton.Checked)
+            {
+                Pen blackPen = new Pen(Color.Black, 4);
+
+                e.Graphics.DrawLine(blackPen, 125, 150, 75, 150);
+                e.Graphics.DrawLine(blackPen, 75, 150, 50, 100);
+                e.Graphics.DrawLine(blackPen, 50, 100, 100, 50);
+                e.Graphics.DrawLine(blackPen, 100, 50, 150, 100);
+                e.Graphics.DrawLine(blackPen, 125, 150, 150, 100);
+
+            }
+            if (fillRadioButton.Checked)
+            {
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                Point point1 = new Point(125, 150);
+                Point point2 = new Point(75, 150);
+                Point point3 = new Point(50, 100);
+                Point point4 = new Point(100, 50);
+                Point point5 = new Point(150, 100);
+
+                Point[] curvePoints = { point1, point2, point3, point4, point5 };
+                e.Graphics.FillPolygon(blackBrush, curvePoints);
+            }
+        }
+        private void hexagon_Paint(object sender, PaintEventArgs e)
+        {
+            if (outlineRadioButton.Checked)
+            {
+                Pen blackPen = new Pen(Color.Black, 4);
+
+                e.Graphics.DrawLine(blackPen, 125, 150, 75, 150);
+                e.Graphics.DrawLine(blackPen, 75, 150, 50,100);
+                e.Graphics.DrawLine(blackPen, 50, 100, 75, 50);
+                e.Graphics.DrawLine(blackPen, 75, 50, 125, 50);
+                e.Graphics.DrawLine(blackPen, 125, 50, 150, 100);
+                e.Graphics.DrawLine(blackPen, 150, 100, 125, 150);
+
+
+            }
+            if (fillRadioButton.Checked)
+            {
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                Point point1 = new Point(125, 150);
+                Point point2 = new Point(75, 150);
+                Point point3 = new Point(50, 100);
+                Point point4 = new Point(75, 50);
+                Point point5 = new Point(125, 50);
+                Point point6 = new Point(150, 100);
+
+                Point[] curvePoints = { point1, point2, point3, point4, point5, point6 };
+                e.Graphics.FillPolygon(blackBrush, curvePoints);
+            }
+        }
+
+        private void nameCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            //Name
+            if (nameCheckBox.Checked)
+            {
+                if (listBox1.SelectedIndex != -1)
+                {
+                    nameLabel.Text = listBox1.SelectedItem.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a shape");
+                    nameCheckBox.Checked = false;
+                }
+            }
+            else
+            {
+                nameLabel.Text = "";
+            }
+        }
+
+        private void dateCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            //Date
+            if (dateCheckBox.Checked)
+            {
+                DateTime thisDay = DateTime.Today;
+                dateLabel.Text = thisDay.ToString("d");
+            }
+            else
+            {
+                dateLabel.Text = "";
+            }
+        }
+    }
+}
